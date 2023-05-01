d943808d3a26c38f6c28cb463b98a852e1f173b2
akinshinn <mouzer665@gmail.com>
2023-05-01 14:41:53 +0300

    optimized time exceed

--- main.cpp
+++ main.cpp
@@ -5,29 +5,30 @@
 
 #include "Control.h"
 
-/// Число лифтов
 const size_t numberOfElevators = 2;
 
-/// Вместимость лифта
-/// \warning Тренироваться проще с меньшей вместимостью, в реальной задаче будет не менее 6 человек
 const size_t elevatorCapacity = 6;
 
-/// Максимальный номер этажа (не считая подвала, который имеет номер 0).
-/// Пассажиры иногда ездят в подвал и из подвала
 const size_t maxFloor = 11;
 
-/// Общее число этажей
 const size_t numberOfFloors = maxFloor + 1;
 
-/// Время моделирования в секундах
-/// \warning Cейчас для тестирования задано 26000 секунд, в реальной задаче может быть, скажем, 54000 секунд: от 7:00 утра до 22:00 вечера
 const size_t maxTime = 6000;
+
+
 struct myParams
 {
     bool started = false;
     size_t* time_table = new size_t[numberOfFloors];
     size_t* time_elv_full = new size_t[numberOfElevators];
     bool* list_elv_full = new bool[numberOfElevators];
+
+        
+    ~myParams() {
+        delete[] time_table, time_elv_full, list_elv_full;
+    }
+
+
     void init_params() {
 
         for (int i = 0; i < numberOfElevators; ++i) {
@@ -47,6 +48,12 @@ struct myParams
         }
     }
 
+    void floorbutton_time_increment_elevator(Control& control, size_t elv) {
+        for (int i = 0; i < numberOfFloors; ++i) {
+            time_table[i] += control.getElevatorButton(elv, i);
+        }
+    }
+
     void floorbutton_time_reset(Control& control, size_t elv) {
         size_t pos = control.getElevatorPosition(elv);
         time_table[pos] = 0;
@@ -63,7 +70,7 @@ struct myParams
         // если лифт заполнен и нажата кнопка в самом лифте
         if (is_elevator_full(control, elv, floor) && control.getElevatorButton(elv, floor)) departure = 1;
 
-        double pr = 1000 * departure + 50 * Indicator + 40 * time_exceed + 30 * distance;
+        double pr = 1000 * departure + 1 * Indicator + 40 * time_exceed + 75 * distance; // min = 69429
 
         return pr;
     }
@@ -177,81 +184,38 @@ struct myParams
 
 void CONTROLSYSTEM(Control& control, myParams& params);
 
-
 int main(int argc, char** argv)
 {
-    //Задание конфигурации лифтового хозяйства
     Control control(numberOfFloors, numberOfElevators, elevatorCapacity);
-    //Для загрузки расписания появления пассажиров из файла
     control.ReadTimeTable("TimeTable/timetable125.csv");
-    //std::cout << "main";
 
-    //Для тестирования вместо чтения из файла (строка выше)
-    //можно вводить появляющихся пассажиров вручную    
-    //параметры в фиг. скобках
-    //{
-    // 1) время появления пассажира (от начала моделирования)
-    // 2) этаж, где появляется пассажир
-    // 3) этаж, куда направляется пассажир
-    // 4) время, которое пассажир ждет и после которого, не вырерживая, уходит (начисляется штраф)
-    // 5) вероятность сесть в лифт, идущий в обратном направлении, в начале ожидания
-    // 6) вероятность сесть в лифт, идущий в обратном направлении, в конце ожидания
-    // 7) вероятеность того, что пассажир, войдя в лифт, нажмет "ход" и лифт не будет стоять
-    // }
-    /*
-    control.AddPassengerToQueue({  5, 5,  3, 300, 0.01, 0.20, 0.50 });
-    control.AddPassengerToQueue({  6, 5, 10, 300, 0.01, 0.20, 0.50 });
-    control.AddPassengerToQueue({  7, 5,  2, 300, 0.01, 0.20, 0.50 });
-    control.AddPassengerToQueue({  8, 5,  8, 300, 0.01, 0.20, 0.50 });
-    control.AddPassengerToQueue({  9, 5, 10, 300, 0.01, 0.20, 0.50 });
-    control.AddPassengerToQueue({ 10, 5,  6, 300, 0.01, 0.20, 0.50 });
-    control.AddPassengerToQueue({ 11, 5,  9, 300, 0.01, 0.20, 0.50 });
-    control.AddPassengerToQueue({ 12, 5,  8, 300, 0.01, 0.20, 0.50 });
-    control.AddPassengerToQueue({ 13, 5, 11, 300, 0.01, 0.20, 0.50 });
-    control.AddPassengerToQueue({ 14, 5, 10, 300, 0.01, 0.20, 0.50 });
-    */
-   /*
-   control.AddPassengerToQueue({1, 11, 10, 10, 0.01, 0.20, 0.50});
-    control.AddPassengerToQueue({ 1, 1, 11, 600, 0.01, 0.20, 0.50 });
-    control.AddPassengerToQueue({ 1, 1, 11, 600, 0.01, 0.20, 0.50 });
-    control.AddPassengerToQueue({ 1, 1, 11, 600, 0.01, 0.20, 0.50 });
-    control.AddPassengerToQueue({ 1, 1, 11, 600, 0.01, 0.20, 0.50 });
-    control.AddPassengerToQueue({ 1, 1, 11, 600, 0.01, 0.20, 0.50 });
-    control.AddPassengerToQueue({ 1, 1, 11, 600, 0.01, 0.20, 0.50 });
-    control.AddPassengerToQueue({ 1, 1, 11, 600, 0.01, 0.20, 0.50 });
-    control.AddPassengerToQueue({ 1, 1, 11, 600, 0.01, 0.20, 0.50 });
-    control.AddPassengerToQueue({ 1, 5, 4, 600, 0.01, 0.20, 0.50 });
-    control.AddPassengerToQueue({ 1, 5, 4, 600, 0.01, 0.20, 0.50 });
-    control.AddPassengerToQueue({ 1, 5, 4, 600, 0.01, 0.20, 0.50 });
-    control.AddPassengerToQueue({ 1, 5, 4, 600, 0.01, 0.20, 0.50 });
-    control.AddPassengerToQueue({ 1, 5, 4, 600, 0.01, 0.20, 0.50 });
-    control.AddPassengerToQueue({ 1, 5, 4, 600, 0.01, 0.20, 0.50 });
-    control.AddPassengerToQueue({ 1, 5, 4, 600, 0.01, 0.20, 0.50 });
-    control.AddPassengerToQueue({ 1, 5, 4, 600, 0.01, 0.20, 0.50 });
-    control.AddPassengerToQueue({ 20, 6, 2, 600, 0.01, 0.20, 0.50 });
-    control.AddPassengerToQueue({ 13, 4, 1, 600, 0.01, 0.20, 0.50 });
-    */
+    //control.AddPassengerToQueue({  5, 5,  3, 300, 0.01, 0.20, 0.50 });
+    //control.AddPassengerToQueue({  6, 5, 10, 300, 0.01, 0.20, 0.50 });
+    //control.AddPassengerToQueue({  7, 5,  2, 300, 0.01, 0.20, 0.50 });
+    //control.AddPassengerToQueue({  8, 5,  8, 300, 0.01, 0.20, 0.50 });
+    //control.AddPassengerToQueue({  9, 5, 10, 300, 0.01, 0.20, 0.50 });
+    //control.AddPassengerToQueue({ 10, 5,  6, 300, 0.01, 0.20, 0.50 });
+    //control.AddPassengerToQueue({ 11, 5,  9, 300, 0.01, 0.20, 0.50 });
+    //control.AddPassengerToQueue({ 12, 5,  8, 300, 0.01, 0.20, 0.50 });
+    //control.AddPassengerToQueue({ 13, 5, 11, 300, 0.01, 0.20, 0.50 });
+    //control.AddPassengerToQueue({ 14, 5, 10, 300, 0.01, 0.20, 0.50 });
+    
+
     myParams params;
     do
     {
-        //Выполнение одного шага (= 1 секунда) моделирования работы лифта
         control.MakeStep();
-        //Вызов функции системы управления --- в ней можно "отдать команду" лифту,
-        //исходя из его текущего состояния и состояния кнопок в лифте и на этажах
         CONTROLSYSTEM(control, params);
 
-        //Вывод состояния лифта
         //control.PrintElevatorState(0);                //Вывод состояния лифта #0 на экран
         //control.PrintElevatorState(1);                //Вывод состояния лифта #1 на экран
 
         control.PrintElevatorState(0, "fileElev0.txt"); //Вывод состояния лифта #0 в файл
         control.PrintElevatorState(1, "fileElev1.txt"); //Вывод состояния лифта #1 в файл
 
-        //Вывод состояния кнопок в лифте и на этажах
         //control.PrintButtonsState();                  //Вывод состояния кнопок на экран
         control.PrintButtonsState("fileButtons.txt");   //Вывод состояния кнопок в файл
 
-        //Вывод событий появлений пассажиров, их входа в лифт, выхода из лифта, ухода с этажа
         //control.PrintPassengerState();                    //Вывод статистики пассажиров на экран
         control.PrintPassengerState("filePassengers.txt");  //Вывод статистики пассажиров в файл
 
@@ -303,10 +267,10 @@ void CONTROLSYSTEM(Control& control, myParams& params)
         if ((params.started) && (control.isElevatorAchievedDestination(elv))) {
      
             params.floorbutton_time_reset(control, elv);
-            for (int i = 0; i < numberOfFloors; ++i) {
-                std::cout << params.time_table[i] << " ";
-            }
-            std::cout << std::endl;
+            //for (int i = 0; i < numberOfFloors; ++i) {
+            //    std::cout << params.time_table[i] << " ";
+            //}
+            //std::cout << std::endl;
 
             size_t pos = (size_t)(control.getElevatorPosition(elv));
             control.unsetDnButton(pos);
@@ -349,11 +313,12 @@ void CONTROLSYSTEM(Control& control, myParams& params)
 
             control.SetElevatorDestination(elv, global_dest);
         }
+        params.floorbutton_time_increment_elevator(control, elv);
 
 
-        params.floorbutton_time_increment(control);
     }
 
+    params.floorbutton_time_increment(control);
 
 
         /* когда-нибудь оно заработает
